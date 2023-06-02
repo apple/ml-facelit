f09bbfbf9cb909fa170f28b9a3ffe5277fe768f8
Anurag Ranjan <anuragr@apple.com>
2023-06-02 12:03:20 -0700

    Update download_models.sh
    Pretrained models added

--- download_models.sh
+++ download_models.sh
@@ -1,4 +1,6 @@
+mkdir pretrained && cd pretrained
 wget https://docs-assets.developer.apple.com/ml-research/models/facelit/ffhq_full.pkl
 wget https://docs-assets.developer.apple.com/ml-research/models/facelit/ffhq_diffuse.pkl
 wget https://docs-assets.developer.apple.com/ml-research/models/facelit/ffhq_128_full.pkl
 wget https://docs-assets.developer.apple.com/ml-research/models/facelit/ffhq_128_diffuse.pkl
+cd ..
